619004fbd9a484951e5a999cad4990d3bf95b9d5
AlkBur <alkboor@mail.ru>
2017-02-17 15:36:55 +0300

    alpha

--- pend_test.go
+++ pend_test.go
@@ -27,3 +27,38 @@ func TestPend(t *testing.T) {
 		t.Fatal(err)
 	}
 }
+
+/*
+========================================
+	BENCHMARKS!!
+========================================
+	# go test -bench=.
+*/
+
+func BenchmarkWithPend(b *testing.B) {
+	p := New()
+	for i := 0; i < b.N; i++ {
+		p.Go(TestingFunc)
+	}
+	err := p.Wait()
+	if err != nil {
+		b.Fatal(err)
+	}
+	//b.Log("OK")
+}
+
+func BenchmarkWithoutPend(b *testing.B) {
+	var err error
+	for i := 0; i < b.N; i++ {
+		err = TestingFunc()
+		if err != nil {
+			b.Fatal(err)
+		}
+	}
+	//b.Log("OK")
+}
+
+func TestingFunc() (err error) {
+	time.Sleep(250 * time.Millisecond)
+	return
+}
